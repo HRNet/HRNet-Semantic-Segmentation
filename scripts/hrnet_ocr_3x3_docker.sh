b6ebcf6d32fdbca665053201e6f1a9dc88f1c435
hsfzxjy <hsfzxjy@gmail.com>
2019-12-04 14:41:04 +0000

    fix bug

--- scripts/hrnet_ocr_3x3_docker.sh
+++ scripts/hrnet_ocr_3x3_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo nvidia-docker run --ipc=host -it -v $HOME/HRNet_OCR-0.4:/workspace/hrnetocr -v /mnt/openseg/data:/mnt/openseg/data  kesun/pose:pytorch-0.4.1-bn-cudnn-off bash -c "cd hrnetocr; pip install -r requirements.txt; python tools/train.py --cfg experiments/cityscapes/seg_hrnet_ocr_bndefault_alignTrue_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484_3x3.yaml"
+sudo nvidia-docker run --ipc=host -it -v $HOME/HRNet_OCR-0.4:/workspace/hrnetocr -v /mnt/openseg/data:/mnt/openseg/data  kesun/pose:pytorch-0.4.1-bn-cudnn-off bash -c "NCCL_LL_THRESHOLD=0 cd hrnetocr; pip install -r requirements.txt; python tools/train.py --cfg experiments/cityscapes/seg_hrnet_ocr_bndefault_alignTrue_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484_3x3.yaml MODEL.PRETRAINED pretrained_models/hrnetv2_w48_imagenet_pretrained.pth"
