99b332162bd447ef0348a165bcdc7def3c96ea3a
hsfzxjy <hsfzxjy@gmail.com>
2019-12-05 08:32:59 +0000

    fix bug

--- scripts/hrnet_ocr_3x3_docker11.sh
+++ scripts/hrnet_ocr_3x3_docker11.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-sudo nvidia-docker run --ipc=host -t -v $HOME/HRNet_OCR:/workspace/hrnetocr -v /mnt/openseg/data:/mnt/openseg/data chaoqw/hrnet_segmentation:pytorchv1.1-cu10_pascal bash -c 'cd /workspace/hrnetocr/; NCCL_LL_THRESHOLD=0 python -m torch.distributed.launch --nproc_per_node=4 tools/train.py --cfg experiments/cityscapes/seg_hrnet_ocr_bndefault_alignTrue_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484_3x3.yaml MODEL.PRETRAINED pretrained_models/hrnetv2_w48_imagenet_pretrained.pth'
-
-
+sudo nvidia-docker run --ipc=host -t -v $HOME/HRNet_OCR-0.4:/workspace/hrnetocr -v /mnt/openseg/data:/mnt/openseg/data chaoqw/hrnet_segmentation:pytorchv1.1-cu10_pascal bash -c 'cd /workspace/hrnetocr/; NCCL_LL_THRESHOLD=0 python -m torch.distributed.launch --nproc_per_node=4 tools/train.py --cfg experiments/cityscapes/seg_hrnet_ocr_bndefault_alignTrue_w48_train_512x1024_sgd_lr1e-2_wd5e-4_bs_12_epoch484_3x3.yaml MODEL.PRETRAINED pretrained_models/hrnetv2_w48_imagenet_pretrained.pth'
