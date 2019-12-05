0aa1896f2f3b43e42cb9d5b446699f862ec721a1
hsfzxjy <hsfzxjy@gmail.com>
2019-12-05 10:19:22 +0000

    fix bug

--- lib/distributed.py
+++ lib/distributed.py
@@ -2,15 +2,15 @@ import torch
 import torch.distributed as torch_dist
 
 def is_distributed():
-    return torch_dist.is_available()
+    return torch_dist.is_initialized()
 
 def get_world_size():
-    if not torch_dist.is_available():
+    if not torch_dist.is_initialized():
         return 1
     return torch_dist.get_world_size()
 
 def get_rank():
-    if not torch_dist.is_available():
+    if not torch_dist.is_initialized():
         return 0
     return torch_dist.get_rank()
 
