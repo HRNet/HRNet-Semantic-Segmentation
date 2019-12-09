284f3696be477caae3ed10b39370ce05deda32e2
hsfzxjy <hsfzxjy@gmail.com>
2019-12-09 06:58:12 +0000

    debug

--- lib/datasets/lip.py
+++ lib/datasets/lip.py
@@ -84,7 +84,11 @@ class LIP(BaseDataset):
                     self.root, 'lip/TrainVal_parsing_annotations/', 
                     item["label"]),
                     cv2.IMREAD_GRAYSCALE)
-        print(type(image), type(label))
+        print(os.path.join(
+                self.root, 'lip/TrainVal_parsing_annotations/', 
+                item["label"]), type(label), label.shape)
+        print(os.path.join(
+                self.root, 'lip/TrainVal_images/', item["img"]), type(image), image.shape, self.crop_size)           
         size = label.shape
 
         if 'testval' in self.list_path:
