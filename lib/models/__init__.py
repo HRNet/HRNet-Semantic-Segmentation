69a36c0033dd87b027c779f34f1e6d3c9047faff
Ubuntu <yuhui@full-resolution-node.cpci2sm3to0ezk22e1vhiccc3h.jx.internal.cloudapp.net>
2019-12-10 09:26:37 +0000

    ocr c

--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -10,5 +10,6 @@ from __future__ import print_function
 
 import models.seg_hrnet
 import models.seg_hrnet_ocr_bndefault_alignTrue_1x1
+import models.seg_hrnet_ocr_c_bndefault_alignTrue_1x1
 import models.seg_hrnet_ocr_bndefault_alignFalse_1x1
 import models.seg_hrnet_ocr_bndefault_alignTrue_3x3
